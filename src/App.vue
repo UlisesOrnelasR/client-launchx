628a03b15281bdcb9e807b8ebc25466953ae6bcf
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-24 13:47:07 -0600

    Agregando ruta en App para agregar un Mission Commander

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
         <li class="nav-item">
           <router-link to="/add" class="nav-link">Agregar Explorer</router-link>
         </li>
+        <li class="nav-item">
+          <router-link to="/addMissionCommander" class="nav-link">Agregar Mission Commander</router-link>
+        </li>
       </div>
     </nav>
     <div class="container mt-3">
